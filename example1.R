3a048b37c4704c674cc3f95644bcd7476ce44d13
Your Nameshi <student64@dee2-dataprocess1.novalocal>
2020-05-16 20:13:16 +1000

    ar

---
+++ example1.R
@@ -0,0 +1,41 @@
+# lets start easy
+# used to put comonts on the scripst 
+1+2
+10000^2
+sqrt(64)
+mean(c(1,10,100)
+ #ctrl +,=<    
+a,<-c(1,10,1000)
+sum(a)
+meatian (a)
+mean(a)
+max(a)
+min(a)
+a = 1
+a
+str(a)
+a<-c(1,3,5,654)
+a
+a[2]
+a[3:4]
+a[c(3,4)]
+a[c(1,3)]
+
+ as.integer(c(1,3,871,5.214,654)
+round(c(1,3,871,5.214,654)) 
+a<-c(1,3.871,5.214,654)
+a
+str(a)
+class(a)
+ typeof(a) 
+ a="x1"
+class(a)
+a<-c(1,3,5,654)
+a[2]
+a{}
+
+is.vector(a)
+is.list(c(1,3.871,5.214,654))
+list(1,3.871,52.214)
+list(c(1,3.871),(c(5.214,654)
+                 
