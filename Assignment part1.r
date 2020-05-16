96d89a5b2e50bf6e1fd5873b07f635e0f9b9e358
Your Nameshi <student64@dee2-dataprocess1.novalocal>
2020-05-16 20:36:41 +1000

    aa

---
+++ Assignment part1.r
@@ -0,0 +1,20 @@
+#function
+sample(1:10,1)
+
+rand_num <- function(){sample(1:10,1) }
+rand_num <- function(){smaple(1:3,1)}
+
+rand_num <- function(){sample(1:10,5) }   
+rand_num <- function()
+rand_num <- function(){sample(1:10,5)}
+rand_num <-function(){sample(1:5,20)}
+rand_num <- function(){sample(1:5,20, replace = TRUE)}
+rand_num()
+
+cubed <-function(x){x^(1/3)}
+cubed root (10)
+
+cubedsum <- function(x,y,e){ z<- cubed(x) +cubed(y)+ cubed(e)}
+cudeedsum(4,6,9)
+x<-sample(-10:10,1)
+x<0
